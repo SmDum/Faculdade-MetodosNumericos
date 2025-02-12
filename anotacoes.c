974776c28c76645676c9266765727f7aff45a5cb
Samuel <samueldemoraesdelgado2005@gmail.com>
2025-02-12 20:12:05 -0300

    Aula 01 - 12/02/2025

---
+++ anotacoes.c
@@ -0,0 +1,19 @@
+/*
+
+    Conteúdo Programático:
+
+    * Método para cálculo de raízes: Dicotomia ou Bissecção.
+    * Método de Interpolação: Polinômio de Lagrange.
+    * Métodos para Resolução de Sistemas Lineares: Eliminação de Gauss.
+    * Método para ajuste de Curvas: Método dos Quadrados Mínimos.
+    * Método de Integração Numérica: Regra dos Trápezios.
+
+    Provas:
+
+    AC1: 02/04
+    AC2: Tarefa 1 - 09/04 | Prova - 14/05 | Tarefa 2 - 21/05
+    AG: 20/05
+    AF: 11/06
+    AS: 25/06
+    
+*/
