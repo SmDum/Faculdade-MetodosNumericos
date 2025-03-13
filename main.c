5dea7ed04f0cee6820d478fec55a27e556432bb1
Samuel de Moraes Delgado <samueldemoraesdelgado2005@gmail.com>
2025-03-13 07:52:12 -0300

    Exercícios

--- main.c
+++ main.c
@@ -19,30 +19,8 @@ void ImprimirCabecalhoTabela();
 void ImprimirTabela(int iteracao, float a, float b, float m, float fA, float fB, float fM, char fAfM, char fMfB);
 float acharPontoMedio(float a, float b);
 
-//-------------------------------
-
-//-------------------------------
 float *multi = NULL;
 int grau;
-
-/*
-PONTEIROS - DICAS
-
-//Criar uma variavel tipo ponteiro
-float *myVar = NULL;
-
-//Alocacao do tamanho solicitado (grau indicado)
-myVar = (float *)malloc(tamanho * sizeof(float));
-
-//Dentro do for para receber os valores
-scanf("%f", myVar+i);
-
-//Acessando os valores do vetor
-//*(myVar+i)
-
-//Liberando a memoria
-//free(myVar);
-*/
 
 int main()
 {
@@ -50,26 +28,17 @@ int main()
     int i, tamanho =7;
     multi = (float *)malloc(tamanho * sizeof(float));
 
-    //printf("%-9i|%8i\n", 19, 12);
-
-
-	//Receber o grau da funcao entre 2 a 6
     printf("Informe o grau da funcao (2 a 6): ");
     scanf("%i", &grau);
 
-	//Receber os multiplicadores. Dica: Armazenar os valores em um vetor.
-	//aX^0 + bX^1 + cX^2 --->Indice do vetor igual ao expoente
     for(i=grau; i>=0; i--, multi++)
     {
         printf("Informe o fator multiplicador de x^%i(%c): ", i, 65+(grau-i));
         scanf("%f", multi);
     }
 
-
-    //Exibindo a funcao recebida
     printf("A funcao recebida foi: ");
 
-// ---------------------------- Mudar para ponteiros ---------------------
     for(i=grau; i>=0; i--)
     {
         if(i==0)
@@ -77,9 +46,6 @@ int main()
         else
             printf("%.2fx^%i + ", *(multi-i-1), i);
     }
-
-    // -------------------------------------------------------------------
-
 
     printf("\n\n");
     system("pause");
@@ -157,8 +123,6 @@ int main()
         printf("\nDicotomia não é válida para o intervalo informado.\n");
     }
 
-
-
     system("pause");
     free (multi);
     return 0;
@@ -169,10 +133,8 @@ float CalculaValorK(float a, float b, float erro)
 {
     float fValorK;
 
-    //Realizar o calculo do valor do K
     fValorK = (log10(b-a) - log10(erro))/(log10(2));
-
-
+    
     return ceil(fValorK);
 }
 
@@ -194,21 +156,13 @@ float funcaoX(float ponto)
 //Imprimindo o cabecalho
 void ImprimirCabecalhoTabela()
 {
-	/*
-    //Impressao do cabecalho
-    printf("I |  a\t|  b\t|  m\t|  f(a)\t|  f(b)\t|  f(m)\t| fa*fm\t| fm*fb\t|\n");
-    printf("--+-----+-------+-------+-------+-------+-------+-------+-------+\n");
-	*/
-	//Impressao do cabecalho
     printf("I |\ta\t|\tb\t|\tm\t|\tf(a)\t|\tf(b)\t|\tf(m)\t|fa*fm\t|fm*fb\t|\n");
     printf("--+-------------+---------------+---------------+---------------+---------------+---------------+-------+-------+\n");
-
 }
 
 //Imprimindo a tabela com os valores
 void ImprimirTabela(int iteracao, float a, float b, float m, float fA, float fB, float fM, char fAfM, char fMfB)
 {
-    //printf("%i |%.2f\t|%.2f\t|%.2f\t|%.2f\t|%.2f\t|%.2f\t|%c\t|%c\t|\n", iteracao, a, b, m, fA, fB, fM, fAfM, fMfB);
 	printf("%i |%.5f\t|%.5f\t|%.5f\t|%.5f\t|%.5f\t|%.5f\t|%c\t|%c\t|\n", iteracao, a, b, m, fA, fB, fM, fAfM, fMfB);
 }
 
